6042895d704d16f09e6a32ba47ac7a740eeb1024
Umberto Pepato <umbo.pepato@gmail.com>
2019-03-11 22:59:36 +0100

    Fix contain background, add page level navigation to navbar on mobile

---
+++ js/contain-sizing.js
@@ -0,0 +1,22 @@
+let container, child, lastHeight;
+
+document.addEventListener('DOMContentLoaded', () => {
+    container = document.querySelector('[data-contain]');
+    console.log('contain', container.dataset.contain);
+    child = document.querySelector(container.dataset.contain);
+    console.log(container, child);
+    updateBgSize();
+});
+
+window.addEventListener('resize', updateBgSize);
+
+function updateBgSize() {
+    if (container && child) {
+        let childBottom = child.getBoundingClientRect().bottom;
+        let newHeight = Math.max(300, childBottom);
+        if (newHeight >= 300 && newHeight !== lastHeight) {
+            container.style.backgroundSize = '100% ' + newHeight + 'px';
+            lastHeight = newHeight;
+        }
+    }
+}
